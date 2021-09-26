265be362180161994818916f0a4c96d1b5bdeb42
Juliano Canuto <julianocanuto@gmail.com>
2021-09-26 20:47:10 -0300

    Project created

---
+++ src/test/java/com/julianocanuto/crud/challenge/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.julianocanuto.crud.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
