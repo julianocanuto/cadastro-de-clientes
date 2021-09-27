387a48b68e427cbc980977ea61b7469a18327826
Juliano Canuto <julianocanuto@gmail.com>
2021-09-27 16:08:56 -0300

    Update Client Entity with ORM

--- src/main/java/com/julianocanuto/crud/challenge/entities/Client.java
+++ src/main/java/com/julianocanuto/crud/challenge/entities/Client.java
@@ -4,14 +4,27 @@ import java.io.Serializable;
 import java.time.Instant;
 import java.util.Objects;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_client")
 public class Client implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String cpf;
 	private Double income;
+	
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
 	private Instant birthDate;
 	private Integer children;
 
