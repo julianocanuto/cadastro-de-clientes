432f1eeec934b7b06b8c4c639e8f5838248f1c37
Juliano Canuto <julianocanuto@gmail.com>
2021-10-03 18:19:26 -0300

    Refactor to fix duplicate code

--- src/main/java/com/julianocanuto/crud/challenge/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/julianocanuto/crud/challenge/resources/exceptions/ResourceExceptionHandler.java
@@ -17,25 +17,27 @@ public class ResourceExceptionHandler {
 
 	@ExceptionHandler(ResourceNotFoundException.class)
 	public ResponseEntity<StandardError> entityNotFound(ResourceNotFoundException e, HttpServletRequest request) {
+		HttpStatus status = HttpStatus.NOT_FOUND;
 		StandardError err = new StandardError();
 		err.setTimestamp(Instant.now());
-		err.setStatus(HttpStatus.NOT_FOUND.value());
+		err.setStatus(status.value());
 		err.setError("Resource not found");
 		err.setMessage(e.getMessage());
 		err.setPath(request.getRequestURI());
-		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+		return ResponseEntity.status(status).body(err);
 
 	}
 	
 	@ExceptionHandler(DatabaseException.class)
 	public ResponseEntity<StandardError> databaseIntegrityViolation(DatabaseException e, HttpServletRequest request) {
+		HttpStatus status = HttpStatus.BAD_REQUEST;
 		StandardError err = new StandardError();
 		err.setTimestamp(Instant.now());
-		err.setStatus(HttpStatus.BAD_REQUEST.value());
+		err.setStatus(status.value());
 		err.setError("Database integrity violation");
 		err.setMessage(e.getMessage());
 		err.setPath(request.getRequestURI());
-		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+		return ResponseEntity.status(status).body(err);
 
 	}
 }
