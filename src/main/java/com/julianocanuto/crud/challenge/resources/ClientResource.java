3b004798a6a00c37bbd5fadb23cb35ff6708e168
Juliano Canuto <julianocanuto@gmail.com>
2021-12-26 16:19:09 -0300

    Add annotation to swagger documentation

--- src/main/java/com/julianocanuto/crud/challenge/resources/ClientResource.java
+++ src/main/java/com/julianocanuto/crud/challenge/resources/ClientResource.java
@@ -7,6 +7,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,14 +22,20 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import com.julianocanuto.crud.challenge.dto.ClientDTO;
 import com.julianocanuto.crud.challenge.services.ClientService;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
 @RestController
 @RequestMapping(value = "/clients")
+@Api(value = "REST API Clients")
+@CrossOrigin(value = "*")
 public class ClientResource {
 
 	@Autowired
 	private ClientService service;
 
 	@GetMapping
+	@ApiOperation(value = "Return a list of pages with all clients register")
 	public ResponseEntity<Page<ClientDTO>> findAll(@RequestParam(value = "page", defaultValue = "0") Integer page,
 			@RequestParam(value = "linesPerPage", defaultValue = "12") Integer linesPerPage,
 			@RequestParam(value = "direction", defaultValue = "ASC") String direction,
@@ -41,6 +48,7 @@ public class ClientResource {
 	}
 
 	@GetMapping(value = "/{id}")
+	@ApiOperation(value = "Return a client register given an Id")
 	public ResponseEntity<ClientDTO> findById(@PathVariable Long id) {
 		ClientDTO clientDTO = service.findById(id);
 
@@ -48,6 +56,7 @@ public class ClientResource {
 	}
 
 	@PostMapping
+	@ApiOperation(value = "Create a new client register")
 	public ResponseEntity<ClientDTO> insert(@RequestBody ClientDTO clientDTO) {
 		clientDTO = service.insert(clientDTO);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(clientDTO.getId())
@@ -56,6 +65,7 @@ public class ClientResource {
 	}
 
 	@PutMapping(value = "/{id}")
+	@ApiOperation(value = "Update a client register given an Id")
 	public ResponseEntity<ClientDTO> update(@PathVariable Long id, @RequestBody ClientDTO clientDTO) {
 		clientDTO = service.update(id, clientDTO);
 
@@ -63,6 +73,7 @@ public class ClientResource {
 	}
 
 	@DeleteMapping(value = "/{id}")
+	@ApiOperation(value = "Delete a client register given an Id")
 	public ResponseEntity<Void> update(@PathVariable Long id) {
 		service.delete(id);
 
